7ca850d436e4b43cc6402308bbe24333490750a8
Murfidar <Murfidar@gmail.com>
2024-11-12 15:09:32 +0700

    chore: update constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,12 +3,13 @@ export const BRI_URL = [
   "https://bridevstudio.bbri.io",
   "https://bridevstudio.dev.bbri.io",
 ];
-export const BASE_URL = "https://example-isolated-app.vercel.app";
+export const BASE_URL = `https://${process.env.BASE_URL}`;
 export const SUPERADMIN_CREDENTIALS = {
   name: "Admin",
-  email: "superadmin@bbri.io",
+  email: "superadmin@bridevstud.io",
 };
 export const ALLOWED_EMAIL_USERS = [
   SUPERADMIN_CREDENTIALS.email,
   "sahrul@feedloop.ai",
+  "murfid@feedloop.ai",
 ];
